07d38fdfb8b6722d0a948b443694f4ff2c11e95c
OlehLuk <oleh.lukianykhin.a@gmail.com>
2019-02-06 18:41:53 +0200

    Added blanks for modules implementation and unit tests

--- data-joiner/src/main/scala/ucu/scala/solar/dj/DJ.scala
+++ data-joiner/src/main/scala/ucu/scala/solar/dj/DJ.scala
@@ -1,5 +1,53 @@
 package ucu.scala.solar.dj
 
-object DJ {
+import java.util.Properties
+import java.util.concurrent.TimeUnit
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.streams.kstream.JoinWindows
+import org.apache.kafka.streams.scala.ImplicitConversions._
+import org.apache.kafka.streams.scala._
+import org.apache.kafka.streams.scala.kstream._
+import org.apache.kafka.streams.{KafkaStreams, StreamsConfig, Topology}
+
+class DJ {
     
+    
+    def jam(): Topology = {
+        import org.apache.kafka.streams.scala.Serdes._
+        
+        val builder: StreamsBuilder = new StreamsBuilder
+        
+        val sensorData: KStream[String, String] = builder.stream[String, String]("sensors")
+        val weather: KStream[String, String] = builder.stream[String, String]("weather")
+        
+        sensorData.join(weather)((v1,v2) => v1+";"+v2,
+            JoinWindows.of(TimeUnit.MINUTES.toMillis(5))).to("mash-up")
+        
+        builder.build()
+    }
+}
+
+object DJ extends App {
+    val props: Properties = {
+        val p = new Properties()
+        p.put(StreamsConfig.APPLICATION_ID_CONFIG, "dj")
+        p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+        p.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+        p
+    }
+    
+    val app = new DJ
+    val topology = app.jam()
+    
+    println(topology.describe)
+    
+    val streams: KafkaStreams = new KafkaStreams(topology, props)
+    
+    streams.cleanUp()
+    streams.start()
+    
+    sys.ShutdownHookThread {
+        streams.close()
+    }
 }
--- generator/src/main/scala/ucu/scala/solar/datagen/DataGen.scala
+++ generator/src/main/scala/ucu/scala/solar/datagen/DataGen.scala
@@ -1,5 +1,39 @@
 package ucu.scala.solar.datagen
 
-object DataGen {
+import java.util.Properties
+import java.util.concurrent.TimeUnit
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer._
+import org.apache.kafka.streams.StreamsConfig
+
+class DataGen(properties: Properties) {
+    val producer = new KafkaProducer[String, String](properties)
     
+    def produce(topic: String, messages: List[List[String]]): Unit = {
+        messages.foreach { m =>
+            producer.send(new ProducerRecord[String, String](topic, m(0), m(1)))
+        }
+        producer.close(100L, TimeUnit.MILLISECONDS)
+    }
+}
+
+
+object DataGen extends App{
+    val props: Properties = {
+        val p = new Properties()
+        p.put(StreamsConfig.APPLICATION_ID_CONFIG, "generator")
+        p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+        p.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+        p.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+        p.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+        p
+    }
+    
+    val generator = new DataGen(props)
+    generator.produce("sensors", List(
+        List("Lviv", "Alles Gut"),
+        List("Sumy", "Winter is comming"),
+        List("London", "is the capital of Great Britain!"),
+    ))
 }
